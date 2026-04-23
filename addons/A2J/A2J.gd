24aa5aef3ad3ab3516b5945d65042e7cdb0a6594
PhosXD <167900279+phosxd@users.noreply.github.com>
2026-04-23 13:14:10 -0400

    Fix typed arrays & dictionaries unable to resolve references.
    - Fixed typed arrays & typed dictionaries not being able to resolve their references & instead being empty. This was caused by the typing of the variable making a brand new array or dictionary when changing it's type, meaning the next pass functions no longer has the correct reference to the variable.

--- addons/A2J/A2J.gd
+++ addons/A2J/A2J.gd
@@ -367,16 +367,19 @@ static func _from_json(value, type_details:Dictionary={}, raw_ruleset:Dictionary
 		if midpoint.call(value, ruleset) == true: return null
 
 	# Convert value.
-	var result = handler.from_json(value, ruleset)
+	var result
 	# Type dictionary.
-	if result is Dictionary:
-		result = A2JUtil.type_dictionary(result, type_details)
+	if type == 'Dictionary':
+		result = handler.from_json(value, ruleset, A2JUtil.type_dictionary({}, type_details))
 	# Type array.
-	elif result is Array:
-		result = A2JUtil.type_array(result, type_details)
+	elif type == 'Array':
+		result = handler.from_json(value, ruleset, A2JUtil.type_array([], type_details))
 	# Type other.
 	elif type_details.get('type') is int:
+		result = handler.from_json(value, ruleset)
 		result = type_convert(result, type_details.get('type'))
+	else:
+		result = handler.from_json(value, ruleset)
 	# Return result.
 	return result
 
